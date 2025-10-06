3219042a8f430536bebf458159536970f1f829e8
Toumazou <yiannistoumazou@gmail.com>
2025-10-06 22:00:47 +0100

    Ch 1: Pygame

---
+++ main.py
@@ -0,0 +1,13 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+# Below line ensures the main() function is only called when this file is run directly; it won't run if it's imported as a module. It's considered the "pythonic" way to structure an executable program in Python. Technically, the program will work fine by just calling main(), but you might get an angry letter from Guido van Rossum if you don't.
+if __name__ == "__main__":
+    main()
